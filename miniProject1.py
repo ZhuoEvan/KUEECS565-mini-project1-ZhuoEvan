92a6efc5377a1f935ae3bc75c561ff60e7c72441
ZhuoEvan <156927177+ZhuoEvan@users.noreply.github.com>
2025-08-29 11:56:28 -0500

    8/29/25 Update 2
    More work on the mini project.

--- miniProject1.py
+++ miniProject1.py
@@ -21,20 +21,20 @@ def encryption(m, K):
 
 #Decryption Function
 def decryption(m, K):
+    m = letterToNum(m)
+    K = letterToNum(K)
     result = m - K % 26
+    result = numToLetter(result)
     return result
 
 #Letter Convertor Function
 def letterToNum(letter):
     num = alphabetDict.get(letter)
-    print(f'letterToNum {num}')
     return num
     
-
 #Number Convertor Function
 def numToLetter(num):
     letter = retrieveKey(num)
-    print(letter)
     return letter
 
 #Dictionary Key Retrieve Function
@@ -42,12 +42,61 @@ def retrieveKey(searchValue):
     for key, value in alphabetDict.items():
         if searchValue == value:
             return key
+    #Error Check: Key Does Not Exist
+    raise RuntimeError("Error 01: Key Does Not Exist Error.")
+
+#Message Split Function
+def splitMessage(message, keyLength):
+    #Local Variables
+    splitMessage = []
+    combinedChar = ''
+    currentIndex = 1
+
+    #Split the Message into keyLength Size
+    for char in message:
+        combinedChar = combinedChar + char
+        if currentIndex % keyLength == 0:
+            splitMessage.append(combinedChar)
+            combinedChar = '' #Reset combinedChar
+        currentIndex += 1 #Increment the currentIndex
     
-    raise RuntimeError("Error 01: Key Does Not Exist Error.") #Error Check
+    #Append Remaining Characters
+    if len(combinedChar) != 0:
+        splitMessage.append(combinedChar)
+    
+    return splitMessage #Return List
+
+#Limit List Function
+def limitList(firstWordLengthList, firstWordLength):
+    #Local Variables
+    firstWords = []
+
+    for word in firstWordLengthList:
+        if len(word) == firstWordLength:
+            firstWords.append(word)
+
+    return firstWords #Return List
+
+#Password Crack Function
+def messageBreak(message, keyLength, firstWordLength, firstWordLengthList):
+    #Local Variables
+    decipherMessages = []
+    firstWords = limitList(firstWordLengthList, firstWordLength)
+    splitmsg = splitMessage(message, keyLength)
+
+    
+
+#File Opener Function
+def fileOpener(file):
+    fileContent = []
+    with open(file) as accessFile:
+        for line in accessFile:
+            fileContent.append(line.strip())
+    return fileContent
 
 #Main Function
 def main():
-    m = encryption('A', 'C')
-    print(m)
+    firstWordLengthList = fileOpener("MP1_dict.txt")
+    messageBreak("MSOKKJCOSXOEEKDTOSLGFWCMCHSUSGX", 2, 6, firstWordLengthList)
 
 main()
